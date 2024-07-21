00ee04b85fad58574d12849224390f8ffb627bec
ayman_nagi <ayman.nagi@dd.sa>
2024-07-21 19:24:43 +0300

    Dockerize Product Microservice

--- ProductMicroService/Program.cs
+++ ProductMicroService/Program.cs
@@ -7,18 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 /* Local Database Context Dependency Injection */
-var dbHost = "localhost";
-var dbName = "product_ms_db";
-var dbPort = "3306";
-var dbUser = "admin";
-var dbPassword = "@12345Admin";
+// var dbHost = "localhost";
+// var dbName = "product_ms_db";
+// var dbPort = "3306";
+// var dbUser = "admin";
+// var dbPassword = "@12345Admin";
 
 /* Docker Database Context Dependency Injection */
-// string dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "productdb";
-// string dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "product_db";
-// string dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "3306";
-// string dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "root";
-// string dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD") ?? "password";
+string dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "productdb";
+string dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "product_db";
+string dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "3306";
+string dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "root";
+string dbPassword = Environment.GetEnvironmentVariable("DB_ROOT_PASSWORD") ?? "123456789";
 
 var connectionString = $"server={dbHost};port={dbPort};database={dbName};user={dbUser};password={dbPassword}";
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(connectionString));
