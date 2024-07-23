46576662f029e85798c30e58739d8d9807c52fc5
ayman_nagi <ayman.nagi@dd.sa>
2024-07-24 01:44:57 +0300

    add auth microservice

---
+++ AuthMicroService/Controllers/AccountsController.cs
@@ -0,0 +1,29 @@
+﻿using JwtAuthenticationManager;
+using JwtAuthenticationManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthMicroService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly JwtTokenHandler _jwtTokenHandler;
+        public AccountsController(JwtTokenHandler jwtTokenHandler)
+        {
+            _jwtTokenHandler = jwtTokenHandler;
+        }
+
+        [HttpPost]
+        public ActionResult<AuthenticationResponse?> Authentication([FromBody] AuthenticationRequest authRequest)
+        {
+            var authResponse = _jwtTokenHandler.GenerateJwtToken(authRequest);
+            if (authResponse == null)
+            {
+                return Unauthorized("Not Authorized");
+            }
+            return authResponse;
+        }
+    }
+}
