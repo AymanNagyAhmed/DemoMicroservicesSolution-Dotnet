f3e6e1b6cb9ddcf20536a4e4f086174c3edf4115
AymanNagyAhmed <AymanNagy.Ahmed@gmail.com>
2024-07-29 18:24:25 +0300

    fix migration error

--- ProductMicroService/AppDbContext.cs
+++ ProductMicroService/AppDbContext.cs
@@ -10,25 +10,25 @@ namespace ProductMicroService
         public DbSet<Product> Products { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            try
-            {
-                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
-                if (databaseCreator != null)
-                {
-                    if (!databaseCreator.CanConnect())
-                    {
-                        databaseCreator.Create();
-                    }
-                    if (!databaseCreator.HasTables())
-                    {
-                        databaseCreator.CreateTables();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            // try
+            // {
+            //     var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+            //     if (databaseCreator != null)
+            //     {
+            //         if (!databaseCreator.CanConnect())
+            //         {
+            //             databaseCreator.Create();
+            //         }
+            //         if (!databaseCreator.HasTables())
+            //         {
+            //             databaseCreator.CreateTables();
+            //         }
+            //     }
+            // }
+            // catch (Exception ex)
+            // {
+            //     Console.WriteLine(ex.Message);
+            // }
         }
 
     }
---
+++ ProductMicroService/Migrations/20240729141137_CreateProductsTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ProductMicroService.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateProductsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "longtext", nullable: false),
+                    code = table.Column<string>(type: "varchar(255)", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_code",
+                table: "products",
+                column: "code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
