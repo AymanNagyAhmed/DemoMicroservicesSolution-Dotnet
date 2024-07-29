df66978825a1e7fad8a8042e8cc7cf604fc32d7a
AymanNagyAhmed <AymanNagy.Ahmed@gmail.com>
2024-07-29 16:32:56 +0300

    Add cors header

--- ProductMicroService/Program.cs
+++ ProductMicroService/Program.cs
@@ -7,18 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 /* Local Database Context Dependency Injection */
-// var dbHost = "localhost";
-// var dbName = "product_ms_db";
-// var dbPort = "3306";
-// var dbUser = "admin";
-// var dbPassword = "@12345Admin";
+var dbHost = "localhost";
+var dbName = "product_ms_db";
+var dbPort = "3306";
+var dbUser = "admin";
+var dbPassword = "@12345Admin";
 
 /* Docker Database Context Dependency Injection */
-string dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "productdb";
-string dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "product_db";
-string dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "3306";
-string dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "root";
-string dbPassword = Environment.GetEnvironmentVariable("DB_ROOT_PASSWORD") ?? "123456789";
+// string dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "productdb";
+// string dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "product_db";
+// string dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "3306";
+// string dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "root";
+// string dbPassword = Environment.GetEnvironmentVariable("DB_ROOT_PASSWORD") ?? "123456789";
 
 var connectionString = $"server={dbHost};port={dbPort};database={dbName};user={dbUser};password={dbPassword}";
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(connectionString));
@@ -28,6 +28,9 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(connecti
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// add cors header
+builder.Services.AddCors();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -39,6 +42,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseAuthorization();
 
 app.MapControllers();
